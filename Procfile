d0d8a8fe8e6bac615abb35f0edaa3ef30fa63c31
kortmiranda <kortneymiranda@outlook.com>
2021-06-13 14:56:30 -0400

    server deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn dreamrest_django.wsgi
--- dreamrest_django/settings.py
+++ dreamrest_django/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=4s4l=t33vl+qrr&kgf_ss8)0x)v#@s40%4v9)@fq-&jc=_tg_'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,8 +45,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -77,14 +80,18 @@ WSGI_APPLICATION = 'dreamrest_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'dreamrest',
+#         'USER': 'dreamrestuser',
+#         'PASSWORD': 'dreamrest',
+#         'HOST': 'localhost'
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dreamrest',
-        'USER': 'dreamrestuser',
-        'PASSWORD': 'dreamrest',
-        'HOST': 'localhost'
-    }
+  'default': dj_database_url.config(conn_max_age=600)
 }
 
 
@@ -148,3 +155,5 @@ CORS_ALLOWED_ORIGINS = [
  "http://127.0.0.1:3000",
  "http://dreamrest.herokuapp.com"
 ]
+
+STATIC_ROOT=os.path.join(BASE_DIR, "static/")
